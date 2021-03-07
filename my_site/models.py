8b209267e5518b7af7faa7efca36ee846d029314
FF <franciscofrancod@gmail.com>
2021-03-07 14:27:03 -0300

    Criando o pojeto

---
+++ my_site/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Produtos(models.Model):
+    data = models.DateField(auto_now_add=True)
+    produto = models.CharField(max_length=40)
+    quantidade = models.DecimalField(max_digits=5, decimal_places=0)
+    preço = models.DecimalField(max_digits=7, decimal_places=2)
+    descrição = models.TextField(blank=False, null=False)
+
+    class Meta:
+
+        verbose_name_plural = "Produtos"
+
+    def __str__(self):
+
+        return self.produto
